599d972170a875a9105e8bc94f34fa56a0897bf3
RyomenDev <aakashvatsh007@gmail.com>
2025-03-16 10:50:14 +0530

    env-sample

--- client/src/api/authApi.jsx
+++ client/src/api/authApi.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const API_BASE_URL = `http://localhost:5000/api/auth`;
 
 export const loginUser = async (email, password) => {
-//   console.log({ email, password });
+  //   console.log({ email, password });
 
   try {
     const response = await axios.post(`${API_BASE_URL}/login`, {
